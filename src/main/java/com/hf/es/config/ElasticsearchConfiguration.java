9b54a6ea78dec42f94065d4ef8efde8e735fb17f
zhanghf <zhanghf@epsoft.com.cn>
2022-07-28 15:38:35 +0800

    客户端初始化，增加用户验证

--- src/main/java/com/hf/es/config/ElasticsearchConfiguration.java
+++ src/main/java/com/hf/es/config/ElasticsearchConfiguration.java
@@ -1,6 +1,15 @@
 package com.hf.es.config;
 
+import org.apache.http.Header;
 import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.apache.http.impl.nio.reactor.IOReactorConfig;
+import org.apache.http.message.BasicHeader;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
@@ -33,17 +42,45 @@ public class ElasticsearchConfiguration {
     @Value("${elasticsearch.connectionRequestTimeout}")
     int connectionRequestTimeout;
 
+    @Value("${elasticsearch.userName}")
+    String userName;
+
+    @Value("${elasticsearch.password}")
+    String password;
+
+
+//    @Bean(destroyMethod = "close", name = "client")
+//    public RestHighLevelClient initRestClient() {
+//        final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+//        credentialsProvider.setCredentials(AuthScope.ANY,
+//                new UsernamePasswordCredentials(userName, password));
+//
+//        RestClientBuilder builder = RestClient.builder(new HttpHost(host, port, "http"))
+//                .setRequestConfigCallback(requestConfigBuilder -> requestConfigBuilder
+//                        .setConnectTimeout(connTimeout)
+//                        .setSocketTimeout(socketTimeout)
+//                        .setConnectionRequestTimeout(connectionRequestTimeout)
+//                ).setMaxRetryTimeoutMillis(5 * 60 * 1000);
+//        return new RestHighLevelClient(builder);
+//    }
+
 
     @Bean(destroyMethod = "close", name = "client")
     public RestHighLevelClient initRestClient() {
+        final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY,
+                new UsernamePasswordCredentials(userName, password));
+
         RestClientBuilder builder = RestClient.builder(new HttpHost(host, port, "http"))
                 .setRequestConfigCallback(requestConfigBuilder -> requestConfigBuilder
                         .setConnectTimeout(connTimeout)
                         .setSocketTimeout(socketTimeout)
                         .setConnectionRequestTimeout(connectionRequestTimeout)
-                ).setMaxRetryTimeoutMillis(5 * 60 * 1000);
+                        //没有x-pack可以不用 setHttpClientConfigCallback
+                ).setHttpClientConfigCallback(httpAsyncClientBuilder -> {
+                    httpAsyncClientBuilder.disableAuthCaching();
+                    return httpAsyncClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+                }).setMaxRetryTimeoutMillis(5 * 60 * 1000);
         return new RestHighLevelClient(builder);
     }
-
-
 }
